a847bffbe30efc5ccadddc09c6d881b9143a6d63
yusuf <yusuffemreecobann@gmail.com>
2023-07-29 01:54:43 +0300

    ödev 3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace patik_odev{
+    class Program{
+        static void Main(string[] args){
+           Console.WriteLine("kaç tane kelime girmek istiyorsunuz: ");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              string[] kelimeler = new string[sayi];
+                for(int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine( (i+1) +". kenimenizi giriniz: ");
+                    kelimeler[i] = Console.ReadLine();
+                }
+                Console.WriteLine("TERSTEN KELİMELERİM:");
+                foreach( string kelimelerim in kelimeler.Reverse())
+                {
+                        Console.Write(kelimelerim + " ");
+                }
+           Console.ReadLine();
+        }
+    }
+}
